f82479e2b79178b004fb68e3a3253b567fbf17e5
ThandoLNjili <thandolnjili@gmail.com>
2019-10-16 11:22:47 +0200

    Updates

---
+++ precache-manifest.8748cec2ebabc1fe8a316dc34cccb058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95b5251eea11dce96dee3f669e5991b",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "dfc9448fc658d7b650b3",
+    "url": "/weatherapp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2ddcfe730670006eb458",
+    "url": "/weatherapp/static/css/main.2f014b30.chunk.css"
+  },
+  {
+    "revision": "dfc9448fc658d7b650b3",
+    "url": "/weatherapp/static/js/2.17b241eb.chunk.js"
+  },
+  {
+    "revision": "2ddcfe730670006eb458",
+    "url": "/weatherapp/static/js/main.0ebb85ff.chunk.js"
+  },
+  {
+    "revision": "6addbc205185a49536cd",
+    "url": "/weatherapp/static/js/runtime-main.608b6d07.js"
+  }
+]);
